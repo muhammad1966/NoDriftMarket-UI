45746ec9dbee0467a8345a26d71e4f1c081d1d70
Muhammad <bsmuhammad352@gmail.com>
2025-11-12 07:44:58 +0200

    feed13

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,5 @@
 
 const hiddenElements = document.querySelectorAll(".postHidden");
-const hiddenTexts = document.querySelectorAll(".textHidden");
 const root = document.querySelector(".feedDiv");
 
 const observer = new IntersectionObserver(enteries => {
@@ -11,4 +10,3 @@ const observer = new IntersectionObserver(enteries => {
 , {root: root });
 
 hiddenElements.forEach(el => observer.observe(el));
-hiddenTexts.forEach(el => observer.observe(el));
